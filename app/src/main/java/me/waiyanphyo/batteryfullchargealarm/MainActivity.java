3b0d4fd40d97fd9e2487c536d90580cd5c7b7c70
waiyanphyo <yuzaki243@gmail.com>
2015-12-21 11:39:07 +0630

    notification test with custom ringtone done

--- app/src/main/java/me/waiyanphyo/batteryfullchargealarm/MainActivity.java
+++ app/src/main/java/me/waiyanphyo/batteryfullchargealarm/MainActivity.java
@@ -1,5 +1,9 @@
 package me.waiyanphyo.batteryfullchargealarm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,17 +16,21 @@ import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtlevel,txtstatus,txtcapacity,txtremain;
+    TextView txtlevel,txtstatus;
+    Button testnoti;
 
 
     @Override
@@ -30,13 +38,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        testnoti = (Button) findViewById(R.id.show_noti);
         txtlevel = (TextView) findViewById(R.id.battery_level);
         txtstatus=(TextView)findViewById(R.id.battery_status);
-        txtcapacity=(TextView)findViewById(R.id.battery_capacity);
-        txtremain=(TextView)findViewById(R.id.battery_remain);
         this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         getBatteryCapacity();
+        testnoti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notify("Battery Fully Charged","unplug your charger");
+                Toast.makeText(getApplicationContext(),"Test Button Click", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    public void Notify(String notificationTitle, String notificationMessage){
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        @SuppressWarnings("deprecation")
+
+//        Notification notification = new Notification(R.mipmap.ic_launcher,"New Message", System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.kyo_nint_tot);
+        Notification noti = new Notification.Builder(getApplicationContext())
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationMessage)
+                .setSmallIcon(R.mipmap.bat_full)
+                .setContentIntent(pendingIntent)
+                .setSound(uri)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setTicker(notificationTitle+"\n"+notificationMessage)
+                .build();
+
+        notificationManager.notify(1, noti);
+    }
+
+//    private void createAndGenerateNotifcation() {
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+//                this).setContentTitle("Testing Notification").setContentText("Unplug your charger");
+//        mBuilder.setAutoCancel(true);
+//        try {
+//            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/"
+//                    + R.raw.kyo_nint_tot);
+//            mBuilder.setSound(uri);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        // Intent resultIntent = new Intent(this, Notification.class);
+//        Intent resultIntent = new Intent(this, MainActivity.class);
+//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//        stackBuilder.addParentStack(Notification.class);
+//        stackBuilder.addNextIntent(resultIntent);
+//        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,
+//                PendingIntent.FLAG_UPDATE_CURRENT);
+//        mBuilder.setContentIntent(resultPendingIntent);
+//        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        mNotificationManager.notify((int) System.currentTimeMillis(),
+//                mBuilder.build());
+//    }
 
     private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
 
@@ -92,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
             // Something went wrong
             e.printStackTrace();
         }
-        txtcapacity.setText(String.valueOf(batteryCapacity));
+//        txtcapacity.setText(String.valueOf(batteryCapacity));
         return batteryCapacity;
     }
 
--- app/src/main/java/me/waiyanphyo/batteryfullchargealarm/PowerConnectionReceiver.java
+++ app/src/main/java/me/waiyanphyo/batteryfullchargealarm/PowerConnectionReceiver.java
@@ -1,5 +1,9 @@
 package me.waiyanphyo.batteryfullchargealarm;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +11,7 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.nfc.Tag;
+import android.os.BatteryManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,7 +24,13 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
     public void onReceive(Context context,Intent intent){
         Uri path = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         Ringtone r = RingtoneManager.getRingtone(context, path);
-//        r.play();
+        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isFull = status == BatteryManager.BATTERY_STATUS_FULL;
+        if (isFull==true) {
+            Notify("Battery Fully charged", "Unplug charger",context);
+            Toast.makeText(context, "Battery Fully Charged",Toast.LENGTH_LONG).show();
+        }
+
         String data = intent.getAction();
         if (data=="android.intent.action.ACTION_POWER_CONNECTED") {
             Toast.makeText(context, "Yeep...Charging now...", Toast.LENGTH_LONG).show();
@@ -28,5 +39,20 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
         }
     }
 
+    private void Notify(String notificationTitle,String notificationMessage,Context context){
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+
+        Uri uri = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.kyo_nint_tot);
+        Notification noti = new Notification.Builder(context.getApplicationContext())
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationMessage)
+                .setSmallIcon(R.mipmap.bat_full)
+                .setSound(uri)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .build();
+
+        notificationManager.notify(0, noti);
+    }
 
 }
